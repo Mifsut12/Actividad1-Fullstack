2d91e22f6bcab0d07a482799fb5d8767c6e61fb9
Ricky <43637211+Mifsut12@users.noreply.github.com>
2026-01-01 22:06:08 +0100

    Delete Relatos-Papel directory

--- Relatos-Papel/src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-export const Footer= () => {
-    return (
-        <footer className = "footer">
-            <p className="footer-footer-text">Creador: Ricardo Mifsut Castilla</p>
-        </footer>
-    );
-}
